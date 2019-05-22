978dbe404eae1aa58569bda3dcda31ea5c08d19f
VladimirTi <tytmes@mail.ru>
2019-05-22 23:19:04 +0300

    correction

--- script.js
+++ script.js
@@ -16,21 +16,22 @@ var data = {
   }
 };
 
-function isEmpty (obj) {return Object.keys(obj).length > 0 ? false : true}
+function isEmpty(obj) {
+  return !!Object.keys(obj).length;
+}
 
-function createHTML (container, obj) {
-  let mainHTML = document.createElement('ul');
-  
+function createHTML(container, obj) {
+  const mainHTML = document.createElement('ul');
+
   for (let key in obj) {
-    let element = document.createElement('li');
-    element.append(key)
-    mainHTML.append(element)
+    const element = document.createElement('li');
+    element.append(key);
+    mainHTML.append(element);
     if (!isEmpty(obj[key])) {
       createHTML(mainHTML, obj[key]);
     }
   }
-  container.append(mainHTML)
+  container.append(mainHTML);
 }
-
 
 createHTML(document.getElementById('tree'), data);
